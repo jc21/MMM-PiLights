ad10b501e867677d01a855c735e130e1027a3aaf
Jamie Curnow <jc@jc21.com>
2017-05-25 20:53:46 +1000

    updates

--- node_helper.js
+++ node_helper.js
@@ -6,8 +6,9 @@ const NodeHelper = require('node_helper');
 const bodyParser = require('body-parser');
 const LPD8806    = require('lpd8806-async');
 const async      = require('async');
+const moment     = require('moment');
 
-var ajv = require('ajv')({
+let ajv = require('ajv')({
     allErrors:   true,
     format:      'full',
     coerceTypes: true
@@ -24,13 +25,13 @@ module.exports = NodeHelper.create({
      * node_helper start method
      */
     start: function() {
-        console.log('[PiLights] Starting node_helper');
+        this.log('Starting node_helper');
 
         this.expressApp.use(bodyParser.json());
         this.expressApp.use(bodyParser.urlencoded({extended: true}));
 
         this.expressApp.get('/PiLights', (req, res) => {
-            console.error('[PiLights] Incoming:', req.query);
+            this.log('Incoming:' + JSON.stringify(req.query));
 
             if (typeof req.query.sequence !== 'undefined') {
                 // Sequence
@@ -66,11 +67,13 @@ module.exports = NodeHelper.create({
      * @param {*}      payload
      */
     socketNotificationReceived: function (notification, payload) {
+        this.log('socketNotificationReceived: ' + notification + ', ' + JSON.stringify(payload), true);
+
         if (notification === 'START') {
             this.config = payload;
 
             try {
-                console.info('Trying to load leds');
+                this.log('Trying to load leds', true);
 
                 // Internal reference to lpd8806-async
                 this.leds = new LPD8806(this.config.ledCount, this.config.device);
@@ -79,17 +82,27 @@ module.exports = NodeHelper.create({
                 this.leds.allOFF();
                 this.leds.setMasterBrightness(this.config.brightness);
 
-                console.log('[PiLights] Leds connected ok');
+                this.log('Leds connected ok', true);
 
             } catch (err) {
-                console.error('[PiLights] Unable to open SPI (' + this.config.device + '), not supported?', err.message);
+                this.log('Unable to open SPI (' + this.config.device + '), not supported? ' + err.message);
                 this.leds = null;
             }
 
         } else if (notification === 'SEQUENCE') {
-            Promise.resolve(this.runSequence(payload)
-                .catch(function (err) {
-                    console.log('[PiLights] Sequence error: ' + err.message);
+            let iterations = 2;
+            let sequence   = payload;
+            let delay      = 0;
+
+            if (typeof payload === 'object') {
+                sequence   = payload.sequence;
+                iterations = payload.iterations || iterations;
+                delay      = payload.delay      || delay;
+            }
+
+            Promise.resolve(this.runSequence(sequence, iterations, delay)
+                .catch((err) => {
+                    this.log('Sequence error: ' + err.message);
                 }));
         }
     },
@@ -99,14 +112,17 @@ module.exports = NodeHelper.create({
      *
      * @param   {String}  sequence
      * @param   {Integer} [iterations]
+     * @param   {Integer} [delay]
      * @returns {Promise}
      */
-    runSequence: function (sequence, iterations) {
-        var self = this;
+    runSequence: function (sequence, iterations, delay) {
+        let self = this;
         iterations = iterations || 2;
 
+        this.log('runSequence: ' + sequence + ', iterations: ' + iterations + ', delay: ' + delay);
+
         return new Promise(function (resolve, reject) {
-            var colors = [0, 0, 0];
+            let colors = [0, 0, 0];
 
             switch (sequence) {
                 case 'blue_pulse':
@@ -136,7 +152,7 @@ module.exports = NodeHelper.create({
                     break;
             }
 
-            resolve(self.pulse(colors[0], colors[1], colors[2], iterations, 20));
+            resolve(self.pulse(colors[0], colors[1], colors[2], iterations, 20, delay));
         });
     },
 
@@ -152,9 +168,7 @@ module.exports = NodeHelper.create({
         this.stopAnimationRequest = true;
 
         if (this.animationRunning) {
-            //console.log('animation was running, delaying new animation');
-
-            var self = this;
+            let self = this;
             setTimeout(function() {
                 self.switchAnimation(cb);
             }, 100);
@@ -169,7 +183,6 @@ module.exports = NodeHelper.create({
      * @returns {Function}
      */
     startAnimation: function (cb) {
-        //console.log('[PiLights] Starting animation..');
         this.stopAnimationRequest = false;
         this.animationRunning = true;
         return cb();
@@ -179,7 +192,6 @@ module.exports = NodeHelper.create({
      *
      */
     stopAnimation: function () {
-        //console.log('[PiLights] Animation stopped.');
         this.stopAnimationRequest = true;
         this.animationRunning = false;
     },
@@ -200,13 +212,20 @@ module.exports = NodeHelper.create({
      * @param {Integer} blue
      * @param {Integer} [iterations]
      * @param {Integer} [speed]
+     * @param {Integer} [delay]
      */
-    pulse: function (red, green, blue, iterations, speed) {
+    pulse: function (red, green, blue, iterations, speed, delay) {
+        delay = delay || 1;
+
+        let self = this;
+
         if (this.leds) {
-            this.switchAnimation(() => {
-                console.log('[PiLights] Pulse (' + red + ',' + green + ', ' + blue +') Iterations: ' + iterations + ', Speed: ' + speed);
-                this.flashEffect(red, green, blue, iterations, speed);
-            });
+            setTimeout(function () {
+                self.switchAnimation(() => {
+                    self.log('Pulse (' + red + ', ' + green + ', ' + blue + ') Iterations: ' + iterations + ', Speed: ' + speed + ', Delay: ' + delay, true);
+                    self.flashEffect(red, green, blue, iterations, speed);
+                });
+            }, delay);
         }
     },
 
@@ -219,7 +238,6 @@ module.exports = NodeHelper.create({
     fillRGB: function(r, g, b) {
         if (this.leds) {
             this.switchAnimation(() => {
-                //console.log('[PiLights] Filling leds with', r, g, b);
                 this.leds.fillRGB(r, g, b);
                 this.stopAnimation();
             });
@@ -231,7 +249,6 @@ module.exports = NodeHelper.create({
      */
     off: function() {
         if (this.leds) {
-            //console.log('[PiLights] Setting Leds Off');
             this.leds.allOFF();
             this.stopAnimation();
         }
@@ -246,15 +263,15 @@ module.exports = NodeHelper.create({
      * @param {Integer} [speed]
      */
     flashEffect: function (r, g, b, iterations, speed) {
-        var self = this;
-        var step = 0.05;
-        var total_iterations = 0;
+        let self = this;
+        let step = 0.05;
+        let total_iterations = 0;
 
         speed      = speed || 10; // ms
         iterations = iterations || 99999;
 
-        var level = 0.00;
-        var dir   = step;
+        let level = 0.00;
+        let dir   = step;
 
         function performStep() {
             if (level <= 0.0) {
@@ -291,6 +308,18 @@ module.exports = NodeHelper.create({
 
         if (this.leds) {
             performStep();
+        }
+    },
+
+    /**
+     * Outputs log messages
+     *
+     * @param {String}  message
+     * @param {Boolean} [debug_only]
+     */
+    log: function (message, debug_only) {
+        if (!debug_only || (debug_only && typeof this.config.debug !== 'undefined' && this.config.debug)) {
+            console.log('[' + moment().format('YYYY-MM-DD HH:mm:ss') + '] [MMM-PiLights] ' + message);
         }
     }
 
